b0ffc977ab3704235f58d78dd2bd3b2ed845076c
nicodichino <nicolasdichino@gmail.com>
2023-08-07 12:37:09 -0300

    FIXED migrations create-user.js CREADO migrations createmuebles.js FIX creacion de tabla muebles en base de datos POST request crea la tabla muebles en base de datos y el mueble!

---
+++ back/migrations/createmuebles.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Muebles', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Muebles');
+  },
+};
