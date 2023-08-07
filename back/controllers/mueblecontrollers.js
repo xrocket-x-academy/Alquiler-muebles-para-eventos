df13c1fc88811e8918137f4e5811aaedcaa914d1
nicodichino <nicolasdichino@gmail.com>
2023-08-06 22:37:31 -0300

    Resueltos problemas de sintaxis en mueblecontrollers

--- back/controllers/mueblecontrollers.js
+++ back/controllers/mueblecontrollers.js
@@ -2,8 +2,8 @@ const muebleProvider = require('../providers/muebleprovider');
 
 exports.createMueble = async (req, res) => {
   try {
-    const { nombre, descripcion, precio } = req.body;
-    const nuevoMueble = await muebleProvider.createMueble(nombre, descripcion, precio);
+    const { name, description, price } = req.body;
+    const nuevoMueble = await muebleProvider.createMueble(name, description, price);
     res.json({ mueble: nuevoMueble, message: 'Creaste un mueble, sos un capo' });
   } catch (error) {
     console.error('No creaste un mueble y no sos un capo:', error);
