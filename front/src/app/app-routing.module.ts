3090668283e67a3b475e468689974d710829ce83
Huilen Solis <xywvanz@gmail.com>
2023-08-08 14:58:23 -0300

    feat(frontend): updated angular version to 15.2.9

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'dashboard'
-  }
 ];
 
 @NgModule({
