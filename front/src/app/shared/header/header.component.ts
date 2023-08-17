dd6ed6238c2c7000e062a83be7c1a514d9381c88
Huilen Solis <xywvanz@gmail.com>
2023-08-17 00:42:26 -0300

    started header

--- front/src/app/shared/header/header.component.ts
+++ front/src/app/shared/header/header.component.ts
@@ -6,15 +6,32 @@ export interface NavLink {
 }
 export interface AuthLink extends NavLink {
   border?: boolean;
-  fillBackground?: boolean
+  fillBackground?: boolean;
+}
+export interface UserInfo {
+  username: string | null;
+  profileImgUrl: string | null;
+  profileUrl: string | null;
+  isUserLogged: boolean;
+  navLinks: NavLink[]
+}
+export interface logoLink extends NavLink {
+  logoUrl: string;
 }
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @Input() navLinks: NavLink[] = []
-  @Input() authLinks: AuthLink[] = []
-  @Input() isUserLogged: boolean = false;
+  @Input() navLinks: NavLink[] = [];
+  @Input() authLinks: AuthLink[] = [];
+  @Input() logoLink: logoLink = { name: 'Home', url: '/', logoUrl: '' };
+  @Input() userInfo!: UserInfo;
+
+  DropDownState = false;
+
+  toggleDropDown() {
+    this.DropDownState = !this.DropDownState;
+  }
 }
