db0642d98ae11e9a82ce9cfd002fe18d4c6f6381
mauricio.basta <mbasta.1402@gmail.com>
2023-08-22 15:13:38 -0300

    indexDB

--- back/models/indexDB.js
+++ back/models/indexDB.js
@@ -3,11 +3,9 @@ const { User } = require('./user');
 const { RoleXUser } = require('./roles_x_user');
 
 // FK_ROLES_X_USER_ROLES
-RoleXUser.belongsTo(Role, { foreignKey: 'role_id' });
-Role.hasOne(RoleXUser, { foreignKey: 'role_id' });
+Role.hasMany(RoleXUser, { as: 'roles', foreignKey: 'role_id' });
 
 // FK_ROLES_X_USER_USERS
-RoleXUser.belongsTo(User, { foreignKey: 'user_id' });
-User.hasOne(RoleXUser, { foreignKey: 'user_id' });
+User.hasMany(RoleXUser, { as: 'roles', foreignKey: 'user_id' });
 
 module.exports = { User, Role, RoleXUser };
