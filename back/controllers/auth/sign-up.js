8dfe2f1178ee1586825b8b56ebd264eb404a48fd
Huilen Solis <xywvanz@gmail.com>
2023-08-27 17:54:37 -0300

    feat(backend): created sign up controller and route

--- back/controllers/auth/sign-up.js
+++ back/controllers/auth/sign-up.js
@@ -1,3 +1,36 @@
-const signUpController = async () => {};
+const jwt = require('jsonwebtoken');
 
+const { userProvider } = require('../../providers/user');
+const { ApiResponse } = require('../../utils/json-response');
+const { HttpsStatusCodes } = require('../../utils/http-status-codes');
+
+const signUpController = async (req, res) => {
+    const { first_name, last_name, username, email, password } = req.body;
+
+    try {
+        const user = await userProvider.create({
+            first_name,
+            last_name,
+            username,
+            email,
+            password,
+        });
+
+        const payload = {
+            sub: user.id,
+        };
+
+        const token = jwt.sign(payload, process.env.SESSION_SECRET, {
+            expiresIn: '2d',
+        });
+        return ApiResponse.success(res, HttpsStatusCodes.CREATED, token);
+    } catch (error) {
+        console.log(error);
+        return ApiResponse.error(
+            res,
+            HttpsStatusCodes.INTERNAL_SERVER_ERROR,
+            'Error while creating the user',
+        );
+    }
+};
 module.exports = { signUpController };
--- back/routes/furniturerentalsroutes.js
+++ back/routes/furniturerentalsroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const furnitureRentalsRouter = express.Router();
 
-const furnitureRentalsController = require('../controllers/furniturerentals');
+const furnitureRentalsController = require('../controllers/furniture/furniturerentals');
 
 furnitureRentalsRouter.post('/', furnitureRentalsController.createFurnitureRental);
 furnitureRentalsRouter.get('/:rentalCode', furnitureRentalsController.getFurnitureRentalByRentalCode);
