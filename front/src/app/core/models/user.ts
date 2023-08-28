2b0ba06f34b0f101ef2a6a5783956ba0ad3bbd1a
mauricio.basta <mbasta.1402@gmail.com>
2023-08-28 16:01:24 -0300

    refactor(frontend): syntaxis + methods

--- front/src/app/core/models/user.ts
+++ front/src/app/core/models/user.ts
@@ -1,33 +1,47 @@
+import { Role } from "./role";
+
 export class User {
     id: number;
-    userName:string ;
-    email:string ;
-    password:string ;
-    firstName:string ;
-    lastName:string ;
-    startDate: Date ;
-    endDate:Date ;
+    userName: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    startDate: Date;
+    endDate: Date;
+    roles: Array<Role>;
 
 
-constructor(id: number, userName:string,email:string, password:string,firstName:string,lastName:string,startDate: Date, endDate:Date)
-{
-    this.id = id;
-    this.userName = userName;
-    this.email = email;
-    this.password = password;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    
-
-}
+    constructor(
+        id: number,
+        userName: string, 
+        email: string, 
+        password: string, 
+        firstName: string, 
+        lastName: string,)
+    {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
 
  //métodos de la clase
- new(){ };
- changeUsername(){ };
- changePassword(){ };
- resetPassword(){ };
- getRoles(){ };
+    changeUsername(newUserName: string): void { this.userName = newUserName };
+    changePassword(oldPassword: string, newPassword: string): void {
+    if(newPassword === oldPassword) {
+        throw new Error(`Password cannot be de same`);
+    }
+
+    this.password = newPassword;
+
+    // posteriormente se envia la instancia de esta clase hacia el service que actualiza
+    // la informacion del usuario
+    };
+    
+    resetPassword(){ };
+    getRoles(): Array<Role> { return this.roles; };
 }
