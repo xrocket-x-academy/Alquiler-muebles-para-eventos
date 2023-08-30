eb71dbdc5412e428119ec3267a2135431bf2ea90
nicodichino <nicolasdichino@gmail.com>
2023-08-30 18:20:43 -0300

    Creado componente y enrutado

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RentalCalculatorComponent } from './modules/rental-calculator/rental-calculator.component';
 
 const routes: Routes = [
 
@@ -7,6 +8,9 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
     pathMatch: 'prefix'
+  },
+  { path: 'calculator',
+   component: RentalCalculatorComponent,
   },
   {
     path: '**',
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { RentalCalculatorComponent } from './modules/rental-calculator/rental-calculator.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    RentalCalculatorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
