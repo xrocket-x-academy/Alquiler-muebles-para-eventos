f29a6c382ef9b4ba57f7ae18a6f7494ca77f27f5
mauricio.basta <mbasta.1402@gmail.com>
2023-09-01 17:23:24 -0300

    refactor(web): refactor signUp and signIn methods in AuthService

--- front/src/app/core/services/auth.service.ts
+++ front/src/app/core/services/auth.service.ts
@@ -5,6 +5,7 @@ import { User } from '../models/user';
 import { Session } from '../models/session';
 import * as moment from 'moment';
 import { ApiService } from '../http/api.service';
+import { ApiResponse } from '../models/api-response';
 
 @Injectable({
   providedIn: 'root'
@@ -16,15 +17,21 @@ export class AuthService implements IAuthService {
   constructor(private apiService: ApiService) { }
 
   signUp(user: User): Observable<Session> {
-    return this.apiService.post<Session>(`${this.controllerPath}/sign-up`, user).pipe(
-      map((response) => {
-        return response;
+    return this.apiService.post<ApiResponse<string>>(`${this.controllerPath}/sign-up`, user).pipe(
+      map((response: ApiResponse<string>) => {
+        const session = new Session(response.data);
+        return session;
       })
     );
   }
   
   signIn(user: User): Observable<Session> {
-    throw new Error('Method not implemented.');
+    return this.apiService.post<ApiResponse<string>>(`${this.controllerPath}/sign-in`, user).pipe(
+      map((response: ApiResponse<string>) => {
+        const session = new Session(response.data);
+        return session;
+      })
+    );
   }
   
   setSession(session: Session): void {
