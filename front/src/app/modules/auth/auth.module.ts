bd91b09c283437c66b010af1cdfe6c5b87026936
Huilen Solis <xywvanz@gmail.com>
2023-09-02 02:50:59 -0300

    feat(frontend): started register form logic and html && css

--- front/src/app/modules/auth/auth.module.ts
+++ front/src/app/modules/auth/auth.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- front/src/app/modules/auth/pages/sign-up/sign-up.component.ts
+++ front/src/app/modules/auth/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
-
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+} from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
+  // passwordMatchValidator(control: AbstractControl): ValidatorFn {
+  //   return
+  // }
 
+  registerForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(50),
+    ]),
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(30),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(30),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(14),
+      Validators.maxLength(16),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(14),
+      Validators.maxLength(16),
+      // this.passwordMatchValidator,
+    ]),
+  });
+
+  onSubmit(): void {
+    console.log('onsubmit');
+  }
 }
