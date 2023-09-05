673dce18a95f660b72b5c347f5faf0f708953838
Soledad Palacio <svpalacio74@gmail.com>
2023-09-05 19:55:26 -0300

    Cambios en componentes list-furniture y details-furniture

--- front/src/app/features/furniture/details-furniture/details-furniture.component.ts
+++ front/src/app/features/furniture/details-furniture/details-furniture.component.ts
@@ -3,6 +3,7 @@ import { Route } from '@angular/router';
 
 import { Furniture } from 'src/app/core/models/furniture';
 import { ActivatedRoute } from '@angular/router';
+import { FurnitureService } from 'src/app/core/services/furniture.service';
 
 @Component({
   selector: 'app-details-furniture',
@@ -10,41 +11,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details-furniture.component.scss']
 })
 export class DetailsFurnitureComponent implements OnInit {
+  furniture: Furniture;
 
-  constructor(private router: ActivatedRoute) {} // Inyectamos el servicio  en el constructor de nuestro componente
+  constructor(private router: ActivatedRoute, private furnitureService: FurnitureService) {} // Inyectamos el servicio  en el constructor de nuestro componente
+
   indice: number =  0;
-  indiceVendedor: number=0;
-  nombre: string='';
-  descripcion:string='';
-  cantidad:number=0;
-  precio: number =0;
-  startFecha: Date = new Date();
-  endFecha: Date= new Date();
-
-  fotos: string[] = [
+     fotos: string[] = [
     '../assets/living1.jpg',
     '../assets/living2.jpg',
     '../assets/living3.jpg',
     '../assets/living1.jpg',
     '../assets/living2.jpg'
-    // Agrega más URLs de fotos aquí
   ];
-
-
   ngOnInit(): void {
 
     this.indice = this.router.snapshot.params ['id'];
-    this.indiceVendedor = this.router.snapshot.params ['idOwner'];
-    this.nombre = this.router.snapshot.params ['name'];
-    this.descripcion = this.router.snapshot.params ['description'];
-    this.cantidad = this.router.snapshot.params ['stock'];
-    this.precio = this.router.snapshot.params ['price'];
-    this.startFecha = this.router.snapshot.params ['startDate'];
-    this.endFecha = this.router.snapshot.params ['endDate'];
-
-    // let furnitureencontrado : Furniture = this.furnitureService.encontrarMueble(this.indice); // no lo tengo
-
-  }
-
-
+    this.furnitureService.getById(this.indice).subscribe(data => {
+    this.furniture = data;
+    });
+    }
 }
--- front/src/app/features/furniture/list-furniture/list-furniture.component.ts
+++ front/src/app/features/furniture/list-furniture/list-furniture.component.ts
@@ -2,6 +2,8 @@ import { Furniture } from 'src/app/core/models/furniture';
 
 import { CardFurnitureComponent } from '../card-furniture/card-furniture.component';
 import { Component, Input, OnInit } from '@angular/core';
+import { FurnitureService } from 'src/app/core/services/furniture.service';
+
 
 @Component({
   selector: 'app-list-furniture',
@@ -10,47 +12,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ListFurnitureComponent implements OnInit {
   @Input() furnitures: any[];
-  constructor() {
+  constructor(private furnitureService:FurnitureService ) {
     this.furnitures = [];
   }
 
   ngOnInit(): void {
-    // cambiar esta implementacion por llamadas al furnitureService
-    this.mockFurnitures();
+    this.obtenerMuebles();
   }
+  private obtenerMuebles(): void {
+    this.furnitureService.getAll().subscribe({
+            next: (furnitures: Furniture[]) => {
+              this.furnitures = furnitures;
+            },
+            error: (error) => {
+              console.error(error);
+            }
+          });
 
-  // MÉTODO QUE AGREGA LO QUE ANTES SE HACIA EN APP COMPONENT
-  private mockFurnitures(): void {
-    // Como no tengo conección con el back ingreso 6 objetos al array furnitures
-    const furniture1= new Furniture(1, 1, "Living", "Sillones 3 cuerpos de gabardina blanca",
-      5, 9000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture1);
-
-    const furniture2= new Furniture(2, 1, "Mesas", "Mesa de pino tamaño 1m", 4, 5000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture2);
-
-    const furniture3= new Furniture(3, 1, "Sillas", "Sillas de pino", 12, 4000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture3);
-
-    const furniture4= new Furniture(4, 1, "Gacebos", "Gacebos blancos", 5, 10000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture4);
-
-    const furniture5= new Furniture(5, 1, "Vajilla", "juego compuesto de 12 cuchillos y 12 tenedores", 20, 7000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture5);
-
-    const furniture6= new Furniture(6, 1, "Sillasplásticas", "Sillas plástcias blancas reforzadas", 10, 3000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture6);
-
-    const furniture7= new Furniture(7, 1, "Gacebos", "Gacebos blancos", 5, 10000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture7);
-
-    const furniture8= new Furniture(8, 1, "Vajilla", "juego compuesto de 12 cuchillos y 12 tenedores", 20, 7000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture8);
-
-    const furniture9= new Furniture(9, 1, "Sillasplásticas", "Sillas plástcias blancas reforzadas", 10, 3000, new Date(2023, 7, 2), new Date(2023, 7, 23));
-    this.furnitures.push(furniture9);
   }
 }
-
-
-
