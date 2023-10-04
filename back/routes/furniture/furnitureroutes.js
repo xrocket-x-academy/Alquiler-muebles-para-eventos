e808ec598f2f0f27d3c77a2f16f9e56ef01217b2
nicodichino <nicolasdichino@gmail.com>
2023-10-04 17:36:20 -0300

    Agregado authentication middleware a furnitureroutes

--- back/routes/furniture/furnitureroutes.js
+++ back/routes/furniture/furnitureroutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const furnitureRouter = express.Router();
 
+const authenticationMiddleware = require('../../middleware/authentication');
+
 const furnitureController = require('../../controllers/furniture/furniturecontrollers');
 
-furnitureRouter.post('/', furnitureController.createFurniture);
-furnitureRouter.get('/all', furnitureController.getAll);
-furnitureRouter.get('/:id', furnitureController.getFurnitureById);
-furnitureRouter.put('/:id', furnitureController.updateFurniture);
-furnitureRouter.put('/update-availability/:id', furnitureController.updateAvailability);
-furnitureRouter.delete('/:id', furnitureController.deleteFurniture);
+furnitureRouter.post('/', authenticationMiddleware, furnitureController.createFurniture);
+furnitureRouter.get('/all', authenticationMiddleware, furnitureController.getAll);
+furnitureRouter.get('/:id', authenticationMiddleware, furnitureController.getFurnitureById);
+furnitureRouter.put('/:id', authenticationMiddleware, furnitureController.updateFurniture);
+furnitureRouter.put('/update-availability/:id', authenticationMiddleware, furnitureController.updateAvailability);
+furnitureRouter.delete('/:id', authenticationMiddleware, furnitureController.deleteFurniture);
 
 module.exports = furnitureRouter;
