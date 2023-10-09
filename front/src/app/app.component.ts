c95a82676efeb848dfba12966fd612fca01c27de
Huilensolis <xywvanz@gmail.com>
2023-10-09 15:18:34 -0300

    feat(server): improve styles

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -19,7 +19,11 @@ export class AppComponent {
       'https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/c22da4223d4282a40e36fec93ba87daf-1547611140034/25a0f27f-8f46-4bf7-ba80-32d7b3085470.png',
     profileUrl: '/user/profile',
     username: 'my username',
-    navLinks: [{ name: 'profile', url: '/user/profile' }, {name: 'my fornitures', url: '/user/fornitures'}, {name: 'test', url: 'test'}],
+    navLinks: [
+      { name: 'profile', url: '/user/profile' },
+      { name: 'my fornitures', url: '/user/fornitures' },
+      { name: 'test', url: 'test' },
+    ],
   };
   navlinks: NavLink[] = [
     {
@@ -28,7 +32,7 @@ export class AppComponent {
     },
     {
       name: 'furniture',
-      url: '/furniture/form',
+      url: '/furniture/list',
     },
   ];
   authLinks: AuthLink[] = [
@@ -44,11 +48,11 @@ export class AppComponent {
     },
   ];
   appLogoUrl: logoLink = {
-    logoUrl: 'https://assets.stickpng.com/images/580b57fcd9996e24bc43c513.png',
+    logoUrl:
+      'https://cdn.icon-icons.com/icons2/2699/PNG/512/airbnb_logo_icon_170605.png',
     name: 'airbn',
     url: '/home',
   };
 
   constructor(private router: Router) {}
 }
-
--- front/src/app/features/furniture/list-furniture/list-furniture.component.ts
+++ front/src/app/features/furniture/list-furniture/list-furniture.component.ts
@@ -4,33 +4,40 @@ import { CardFurnitureComponent } from '../card-furniture/card-furniture.compone
 import { Component, Input, OnInit } from '@angular/core';
 import { FurnitureService } from 'src/app/core/services/furniture.service';
 
-
 @Component({
   selector: 'app-list-furniture',
   templateUrl: './list-furniture.component.html',
-  styleUrls: ['./list-furniture.component.scss']
+  styleUrls: ['./list-furniture.component.scss'],
 })
-export class ListFurnitureComponent implements OnInit {
-
-  @Input() furnitures: Furniture[];
-
-  constructor(private furnitureService: FurnitureService) {
-    this.furnitures = [];
-  }
-
-  ngOnInit(): void {
-    this.obtenerMuebles();
-  }
-  private obtenerMuebles(): void {
-    this.furnitureService.getAll().subscribe({
-      next: (furnitures: Furniture[]) => {
-        // Type missmatch.
-        this.furnitures = furnitures;
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
-
-  }
+export class ListFurnitureComponent {
+  furnitures = [
+    new Furniture(
+      1,
+      1,
+      'my name',
+      'my description',
+      1,
+      123,
+      new Date(),
+      new Date()
+    ),
+  ];
+  // @Input() furnitures: Furniture[];
+  // constructor(private furnitureService: FurnitureService) {
+  //   this.furnitures = [];
+  // }
+  // ngOnInit(): void {
+  //   this.obtenerMuebles();
+  // }
+  // private obtenerMuebles(): void {
+  //   this.furnitureService.getAll().subscribe({
+  //     next: (furnitures: Furniture[]) => {
+  //       // Type missmatch.
+  //       this.furnitures = furnitures;
+  //     },
+  //     error: (error) => {
+  //       console.error(error);
+  //     }
+  //   });
+  // }
 }
